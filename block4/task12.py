d1be663ff98236c650dc3ea437af481e75a73e7b
Boss <kosevaa@mail.ru>
2025-07-21 13:35:35 +0300

    qasdf

---
+++ block4/task12.py
@@ -0,0 +1,10 @@
+a=int(input())
+b=int(input())
+c=int(input())
+m=a
+if b>m:
+    m=b
+if c>m:
+    m=c
+print(m)
+
---
+++ block5/task3.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+for x in range(1,10):
+
+         print(x)
